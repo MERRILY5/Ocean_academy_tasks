3ffd9af674479142683f1c0255875ad2db982517
Merwin <122204469+MERRILY5@users.noreply.github.com>
2024-04-12 20:04:50 +0530

    Add files via upload

---
+++ Test 1.py
@@ -0,0 +1,40 @@
+
+##Simple for-loop
+
+##a=input("Enter a string: ")
+##for i in a:
+##    if " " == i:
+##        print(end="")
+##    else:
+##        print(i,end="")
+
+
+
+##for i in range(1,6):
+##    for j in range(1,i+1):
+##        print(j,end=" ")
+##    print()
+
+
+
+##p=1
+##for i in range(1,5):
+##    for j in range(1,i+1):
+##        print(p,end=" ")
+##        p+=1
+##    print()
+
+
+
+##Prime number
+
+##num=int(input("Enter a number: "))
+##count=0
+##for i in range(1,num+1):
+##    if (num%1==0) and (num%i==0):
+##        count+=1
+##        
+##if count==2:
+##    print("Prime Number")
+##else:
+##    print("Not a Prime Number")
